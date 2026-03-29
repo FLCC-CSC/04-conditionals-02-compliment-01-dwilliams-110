a37d69a3991bb2c181c37591895992854fa7251b
Daniel Williams <dwilliams110@fingerlakes.edu>
2026-03-29 04:23:25 +0000

    compliment the user

--- compliment_01.py
+++ compliment_01.py
@@ -1,8 +1,8 @@
 # FILE NAME - compliment_01.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Daniel Williams
+# DATE: 3/26/2026
+# BRIEF DESCRIPTION:  Compliment the User
 
 
 
@@ -15,12 +15,12 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+compliment = input('Would you like a compliment?: ')
 
+if compliment == 'yes':
+    print('You have wonderful eyes.')
 
-
-
-
-
+print('Thank you for playing.')
 
 
 
@@ -65,12 +65,12 @@ Thank you for playing.
 
 '''
 
-1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab?
+1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab? I would rate this lab a 3
 
 
 
 
-2. What was the hardest part of this lab?
+2. What was the hardest part of this lab? The hardest part of this lab was
 
 
 
